321ee0b1864c96c1359c3a80180bb0e786d60014
kolosfurmanz <kolosfurmanz@ufl.edu>
2022-12-05 18:18:49 -0500

    updated distance-redshift code using master csv

---
+++ 2137_rdcode.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import pandas as pd
+data = pd.read_csv('data.csv', delimiter = ',', usecols = (5,6), skiprows = np.arange(52, 264))
+data = data.fillna(value = 0)
+data = data[:].values
+z = []
+for i in data:
+    z.append(i[0])
+z.pop(-1)
+z = np.sort(z)
+H_0 = (69.8 + 74) / 2
+c = 299792.458
+def d(z):
+    return ((z*c*(z + 2)) / ((z + 1)**2 + 1)) / H_0
+plt.scatter(z, d(z))
+plt.xlabel("Redshift (z)")
+plt.ylabel("Distance (Mpc)")
+plt.title("Distance v.s. Redshift for Plate 2137 (nQSOs = 51)")
+plt.savefig('2137_rd_graph.png')
