0febdf11982157b8c19f5773c7c21c9217dfe3e3
kolosfurmanz <kolosfurmanz@ufl.edu>
2022-12-05 17:47:39 -0500

    absolute magnitude v.s. redshift code for 2137

---
+++ 2137magcode.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+data = pd.read_csv('data.csv', delimiter = ',', usecols = (5,9), skiprows = np.arange(52, 264))
+extinction = pd.read_csv('data.csv', delimiter = ',', usecols = (7,8), skiprows = np.arange(52, 264))
+data = data.fillna(value = 0)
+extinction = extinction.fillna(value = 0)
+data = data[:].values
+extinction = extinction[:].values
+z = []
+r = []
+A_v = []
+
+for i in data:
+    z.append(i[0])
+    r.append(i[1])
+for j in extinction: 
+    A_v.append(j[0])
+z.pop(-1)
+r.pop(-1)
+A_v.pop(-1)
+z = np.array(z)
+r = np.array(r)
+A_v = np.array(A_v)
+H_0 = (69.8 + 74) / 2
+c = 299792.458
+def M(z, r, A_v):
+    d = ((z*c*(z + 2)) / ((z + 1)**2 + 1)) / H_0
+    return r - 5*np.log10(d*10**5) - A_v
+plt.scatter(z, M(z, r, A_v))
+plt.xlabel("Redshift (z)")
+plt.ylabel("Absolute Magnitude")
+plt.title("Absolute Magnitude v.s. Redshift for Plate 2137 (nQSOs = 51)")
+plt.savefig("2137_mag_graph.png")
