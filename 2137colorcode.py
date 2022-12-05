a75aa28472dce03f0562ea63c19c7874c1df0c7e
kolosfurmanz <kolosfurmanz@ufl.edu>
2022-12-05 16:54:20 -0500

    updated 2137 color-color code that uses master csv

--- 2137colorcode.py
+++ 2137colorcode.py
@@ -1,8 +1,12 @@
 from matplotlib import pyplot as plt, colors
 import numpy as np
 import pandas as pd
-data = np.loadtxt('2137-color-data.csv', dtype = 'float', delimiter = ',', skiprows = 1)
-scale = np.loadtxt('2137-zr-data.csv', dtype = 'float', delimiter = ',', skiprows = 1)
+data = pd.read_csv('data.csv', delimiter = ',', usecols = (8, 9), skiprows = np.arange(52, 264))
+scale = pd.read_csv('data.csv', delimiter = ',', usecols = (5, 6), skiprows = np.arange(52, 264))
+data = data.fillna(value = 0)
+scale = scale.fillna(value = 0)
+data = data[:].values
+scale = scale[:].values
 r_i = []
 i_z = []
 z = []
@@ -11,6 +15,9 @@ for i in data:
     i_z.append(i[1])
 for j in scale:
     z.append(j[0])
+z.pop(-1)
+r_i.pop(-1)
+i_z.pop(-1)
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 x = r_i
@@ -27,5 +34,5 @@ cbar = fig.colorbar(sm)
 cbar.set_label("Redshift (z)")
 plt.xlabel('r-i')
 plt.ylabel('i-z')
-plt.title('r-i v.s. i-z Color Filters for Plate 2137 (nQSOs = 51)')
+plt.title('i-z v.s. r-i Color Filters for Plate 2137 (nQSOs = 51)')
 plt.savefig('2137_color-color_graph.png')
