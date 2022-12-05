200cf32c4374fbe51b4686329764ceb670291142
kolosfurmanz <kolosfurmanz@ufl.edu>
2022-12-05 17:10:32 -0500

    updated colormap code for z for 2958 color-color graph

--- 2958colorcode.py
+++ 2958colorcode.py
@@ -1,8 +1,10 @@
 from matplotlib import pyplot as plt, colors
 import numpy as np
 import pandas as pd
-data = np.loadtxt('2958-color-data.csv', dtype = 'float', delimiter = ',', skiprows = 1)
-scale = np.loadtxt('2958-zr-data.csv', dtype = 'float', delimiter = ',', skiprows = 1)
+data = pd.read_csv('data.csv', delimiter = ',', usecols = (13, 14))
+scale = pd.read_csv('data.csv', delimiter = ',', usecols = (10, 11))
+data = data[:].values
+scale = scale[:].values
 r_i = []
 i_z = []
 z = []
@@ -13,12 +15,15 @@ for j in scale:
     z.append(j[0])
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
+np.flip(r_i, 0)
+np.flip(i_z, 0)
+np.flip(z, 0)
 x = r_i
 y = i_z
 c = z
 df = pd.DataFrame({"x": x, "y": y, "c": c})
 fig, ax = plt.subplots()
-cmap = plt.cm.RdBu
+cmap = plt.cm.RdBu_r
 norm = colors.Normalize(vmin = np.min(z), vmax = np.max(z))
 ax.scatter(df.x, df.y, color=cmap(norm(df.c.values)))
 ax.set_facecolor('grey')
@@ -27,5 +32,5 @@ cbar = fig.colorbar(sm)
 cbar.set_label("Redshift (z)")
 plt.xlabel('r-i')
 plt.ylabel('i-z')
-plt.title('r-i v.s. i-z Color Filters for Plate 2958 (nQSOs = 264)')
+plt.title('i-z v.s. r-i Color Filters for Plate 2958 (nQSOs = 264)')
 plt.savefig('2958_color-color_graph.png')
