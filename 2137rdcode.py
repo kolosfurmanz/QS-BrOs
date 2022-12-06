77f1af2dde8244790c4ed573b59e523dcfb8c07d
kolosfurmanz <kolosfurmanz@ufl.edu>
2022-12-06 15:17:03 -0500

    added error bars

--- 2137rdcode.py
+++ 2137rdcode.py
@@ -13,8 +13,16 @@ H_0 = (69.8 + 74) / 2
 c = 299792.458
 def d(z):
     return ((z*c*(z + 2)) / ((z + 1)**2 + 1)) / H_0
+def highd(z):
+    return ((z*c*(z + 2)) / ((z + 1)**2 + 1)) / 69.8
+def lowd(z):
+    return ((z*c*(z + 2)) / ((z + 1)**2 + 1)) / 74
+error = highd(z) - lowd(z)
 plt.scatter(z, d(z))
+plt.errorbar(z, d(z), yerr = error, fmt = "o")
 plt.xlabel("Redshift (z)")
 plt.ylabel("Distance (Mpc)")
 plt.title("Distance v.s. Redshift for Plate 2137 (nQSOs = 51)")
 plt.savefig('2137_rd_graph.png')
+mean_d = np.sum(d(z)) / len(d(z))
+print("the average distance is", mean_d, "Mpc")
